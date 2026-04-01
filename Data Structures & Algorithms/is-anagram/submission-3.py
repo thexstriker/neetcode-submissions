712d1bc1b04512e4e91857f4328f60ca1c1b4b53
Sean Siwon Kim <122237931+thexstriker@users.noreply.github.com>
2026-04-01 12:51:15 -0400

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        listS = list(s)
+        listT = list(t)
+
+        if(len(listS) != len(listT)):
+            return False
+        
+        dictS = {}
+        dictT = {}
+
+        for char in listS:
+            dictS[char] = dictS.get(char, 0) + 1
+        for char in listT:
+            dictT[char] = dictT.get(char, 0) + 1
+            
+        for i in dictS:
+            if(dictS.get(i) != dictT.get(i)):
+                return False
+        return True
+
+
+
+        
+        
